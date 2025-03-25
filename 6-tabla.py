80f348b0a099b78c2c38ec95c1a2c1f933527f4a
Agustin Monardes <110945577+A-monardes@users.noreply.github.com>
2025-03-25 19:16:56 -0300

    Subido a la carpeta unidad 1

--- 6-tabla.py
+++
@@ -1,3 +0,0 @@
-num = int(input("Ingrese un numero:"))
-
-print(f"La tabla del {num} es:\n{num}x1= {num*1}\n{num}x2= {num*2}\n{num}x3= {num*3}\n{num}x4= {num*4}\n{num}x5= {num*5}\n{num}x6= {num*6}\n{num}x7= {num*7}\n{num}x8= {num*8}\n{num}x9= {num*9}\n{num}x10= {num*10}")
